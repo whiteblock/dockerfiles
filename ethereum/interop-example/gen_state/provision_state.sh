2ef37c837df18c2c8fe19c7418626f868974ad73
Daniel <choidanielw@gmail.com>
2019-09-08 15:02:10 -0400

    add readme instruction + add arguements for provision

--- ethereum/interop-example/gen_state/provision_state.sh
+++ ethereum/interop-example/gen_state/provision_state.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-docker build . -t zcli &>/dev/null
+[ -z $1 ] && echo "no count argument given" && exit
+[ -z $2 ] && echo "no genesis-time argument given" && exit
+
 echo "building docker image"
+docker build . -t zcli &>/dev/null
+
+echo "generating state -- count:"$1"; out:./state.ssz; genesis-time:"$2
 
 {
     docker container run -id --name zcli zcli &>/dev/null
-    docker exec zcli zcli genesis mock --count 60 --keys ./keygen_10000_validators.yaml --out ./state.ssz
+    docker exec zcli zcli genesis mock --count $1 --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $2 &>/dev/null
 } || {
 
-    docker exec zcli zcli genesis mock --count 60 --keys ./keygen_10000_validators.yaml --out ./state.ssz
+    docker exec zcli zcli genesis mock --count $1 --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $2 &>/dev/null
 }
 
 docker container cp zcli:/zcli/state.ssz ./
