35f5e4072c14e60c14fac60f92e4f458a36452c6
Daniel <choidanielw@gmail.com>
2019-09-11 22:01:26 -0400

    remove keys before copying

--- ethereum/interop-example/gen_state/provision_state.sh
+++ ethereum/interop-example/gen_state/provision_state.sh
@@ -16,11 +16,15 @@ do
     SHARDS+=" "
 done
 
+rm key_batch*
+
 {
     sudo docker container run -id --name zcli zcli &>/dev/null
+    sudo docker exec zcli bash -c "rm ./key_batch*"
     sudo docker exec zcli zcli keys shard --keys keygen_10000_validators.yaml $SHARDS
     sudo docker exec zcli zcli genesis mock --count $(($1*$3)) --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $2 &>/dev/null
 } || {
+    sudo docker exec zcli bash -c "rm ./key_batch*"
     sudo docker exec zcli zcli keys shard --keys keygen_10000_validators.yaml $SHARDS
     sudo docker exec zcli zcli genesis mock --count $(($1*$3)) --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $2 &>/dev/null
 }
