35bffbbccaac16ba7798e5b0cac3ea9605ccbda1
Antoine Toulme <antoine@lunar-ocean.com>
2019-09-12 09:15:38 -0400

    Add unix time stamp of current clock to genesis time delay

--- ethereum/interop-example/gen_state/provision_state.sh
+++ ethereum/interop-example/gen_state/provision_state.sh
@@ -4,10 +4,13 @@
 [ -z $2 ] && echo "no genesis-time argument given" && exit
 [ -z $3 ] && echo "no shards argument given" && exit
 
+TIME=$(date +%s)
+TIME=$(($TIME+$2))
+
 echo "building docker image"
 sudo docker build . -t zcli &>/dev/null
 
-echo "generating state -- count:"$1"; out:./state.ssz; genesis-time:"$2"; shards:"$3
+echo "generating state -- count:"$1"; out:./state.ssz; genesis-time:"$TIME"; shards:"$3
 
 SHARDS=""
 for (( i=0; i<$3; i++ ))
@@ -23,11 +26,11 @@ rm state.ssz
     sudo docker container run -id --name zcli zcli &>/dev/null
     sudo docker exec zcli bash -c "rm ./key_batch* state.ssz"
     sudo docker exec zcli zcli keys shard --keys keygen_10000_validators.yaml $SHARDS
-    sudo docker exec zcli zcli genesis mock --count $(($1*$3)) --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $2 &>/dev/null
+    sudo docker exec zcli zcli genesis mock --count $(($1*$3)) --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $TIME &>/dev/null
 } || {
     sudo docker exec zcli bash -c "rm ./key_batch* state.ssz"
     sudo docker exec zcli zcli keys shard --keys keygen_10000_validators.yaml $SHARDS
-    sudo docker exec zcli zcli genesis mock --count $(($1*$3)) --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $2 &>/dev/null
+    sudo docker exec zcli zcli genesis mock --count $(($1*$3)) --keys ./keygen_10000_validators.yaml --out ./state.ssz --genesis-time $TIME &>/dev/null
 }
 
 sudo docker container cp zcli:/zcli/state.ssz ./
